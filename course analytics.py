1530b4f931cee3f39f72265ed6b877ffbc123bf1
Bennett Brown <bennettbrowniowa@users.noreply.github.com>
2020-04-15 11:52:36 -0500

    abstract paginate, just a shell of it
    In[]: a
    Out[]: [22183, 22694, 22197, 22542, 22544, 22700, 22195, 22649, 22805, 22626]

--- course analytics.py
+++ course analytics.py
@@ -13,12 +13,20 @@ def get_api_response(api_endpoint):
                        '/enrollments'
     auth = {'Authorization': 'Bearer '+token}
     response = requests.get(api_endpoint_url, headers=auth)
-    data=json.loads(response.content)
+    first_response_json=json.loads(response.content)
     #response.headers has a 'next' item
-    while False: #for now
-        pass
+    data = paginate(first_response_json)
     return data
-    
+
+import copy #There has to be a better way to do this
+def paginate(first_response_json):
+    #return the first response JSON, with all the following pages 
+    #of the server's paginated response linked in the ['links'].split(';').['next']
+    aggregate_response_json = copy.copy(first_response_json)#list.copy() doesn't exist until Python 3.3
+    while False: #while theres a link and a next
+        pass #aggregate
+    return aggregate_response_json    
+
 def get_student_userids_in_course(courseid):
     api_endpoint = '/api/v1/courses/'+ courseid + '/enrollments'
     data = get_api_response(api_endpoint)            
