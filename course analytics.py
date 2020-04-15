a879eb3dd60fd589ebd06b85e8f26aef6144b4d7
Bennett Brown <bennettbrowniowa@users.noreply.github.com>
2020-04-15 13:42:14 -0500

    paginate
    It only gets the 2nd page for some reason, but it does work for that!

--- course analytics.py
+++ course analytics.py
@@ -1,7 +1,11 @@
 import requests
 import json
-from secret import * # set authorization token, stored in a separate file listed in git.ignore
-#token='now set securely from secret'
+
+from api_token import * # set authorization token, stored in a separate file listed in git.ignore
+#token is now set using another file which can be kept more securely than this code.
+#Create a file api_token.py continaing the following line of code, uncommented:
+#token='10577~jhfjdashfjkdhafljhdajkhflsjkdhafjahlruthSDGgdfijdf'
+
 baseurl='https://iowacityschools.instructure.com'
 courseid='32536' #applied tech
 courseid2='32512' #CS tech
@@ -15,16 +19,26 @@ def get_api_response(api_endpoint):
     response = requests.get(api_endpoint_url, headers=auth)
     first_response_json=json.loads(response.content)
     #response.headers has a 'next' item
-    data = paginate(first_response_json)
+    data = paginate(first_response_json, response.headers)
     return data
 
 import copy #There has to be a better way to do this
-def paginate(first_response_json):
+def paginate(first_response_json, response_header_json):
     #return the first response JSON, with all the following pages 
     #of the server's paginated response linked in the ['links'].split(';').['next']
     aggregate_response_json = copy.copy(first_response_json)#list.copy() doesn't exist until Python 3.3
-    while False: #while theres a link and a next
-        pass #aggregate
+    while 'Link' in response_header_json: #while theres a link and a next
+        for link in response_header_json['Link'].split(','):
+            if 'next' in link:
+                link=link[1:link.find('>')]
+                auth = {'Authorization': 'Bearer '+token}
+                response = requests.get(link, headers=auth)
+                response_json = json.loads(response.content)
+                response_header_json = response.headers
+                aggregate_response_json += response_json
+                print response_header_json['Link']
+            else:    
+                response_header_json=''
     return aggregate_response_json    
 
 def get_people_in_course(courseid):
@@ -39,5 +53,5 @@ def get_people_in_course(courseid):
         roles.append(student['role'])       
     return user_ids, names, roles
                            
+#a = get_people_in_course(courseid)
 user_ids, names, roles = get_people_in_course(courseid)
-        
