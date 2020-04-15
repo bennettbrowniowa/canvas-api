59809df06825c7c82598fdcb85c10ff3614c0c20
Bennett Brown <bennettbrowniowa@users.noreply.github.com>
2020-04-15 11:01:26 -0500

    get student ids in a course
    After course_analytics.py executes:
    
    In[]: a
    Out[]: [22183, 22694, 22197, 22542, 22544, 22700, 22195, 22649, 22805, 22626]

--- course analytics.py
+++ course analytics.py
@@ -1,11 +1,12 @@
 import requests
 import json
-token='10577~CIeTyXGQoM6VdBWhCQqhV2uxHsZOnA3Qs8O5Cyy1ZIdbBidIHG7qiTNkPYHcetuk'
+from secret import * # set authorization token, stored in a separate file listed in git.ignore
+#token='now set securely from secret'
 baseurl='https://iowacityschools.instructure.com'
 courseid='32536' #applied tech
 courseid2='32512' #CS tech
 
-def get_student_userids_in_course(courseid):
+def get_api_response(api_endpoint):
     api_endpoint_url = baseurl + \
                        '/api/v1/courses/'+ \
                        courseid+ \
@@ -13,9 +14,17 @@ def get_student_userids_in_course(courseid):
     auth = {'Authorization': 'Bearer '+token}
     response = requests.get(api_endpoint_url, headers=auth)
     data=json.loads(response.content)
+    #response.headers has a 'next' item
+    while False: #for now
+        pass
+    
+def get_student_userids_in_course(courseid):
+    api_endpoint = '/api/v1/courses/'+ courseid + '/enrollments'
+    get_api_response(api_endpoint)            
     ids=[]
     for student in data:
         ids.append(student['user_id'])
     return ids
 
+                       
 a = get_student_userids_in_course(courseid)    
